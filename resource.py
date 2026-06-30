a010e5cca9c1a634847f1b2e84a11c4c6c80104b
KerselRanch <jordanrcooklol@gmail.com>
2026-06-30 15:58:49 +1000

    minor tweaks

--- resource.py
+++ resource.py
@@ -19,7 +19,7 @@ async def gather(self, message):
         embed = discord.Embed(title="🪨 Mining Rock... 🪨")
         await message.reply(embed = embed)
         action_using[message.author.name] = True
-        await asyncio.sleep(10)
+        await asyncio.sleep(randint(7,10))
         action_using[message.author.name] = False
         embed.title="🪨 You Mined a Rock! 🪨"
         await message.reply(embed = embed)
@@ -28,7 +28,7 @@ async def gather(self, message):
         embed = discord.Embed(color = discord.Colour.dark_orange(), title="🪵 Chopping Wood... 🪵")
         await message.reply(embed = embed)
         action_using[message.author.name] = True
-        await asyncio.sleep(10)
+        await asyncio.sleep(randint(7,10))
         action_using[message.author.name] = False
         embed.title="🪵 You Chopped some Wood! 🪵"
         await message.reply(embed = embed)
